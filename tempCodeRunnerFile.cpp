6171dfffe0b6991da440052537f57084b184b80f
SciencePotato <houchic@bu.edu>
2024-01-19 13:54:47 -0500

    Added Git ignore

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-    ifstream inputFile ("testFiles/basictTest.txt");
-    string line;
-    if (inputFile.is_open()) {
-        while (inputFile) {
-            getline(inputFile, line);
-            cout << line << '\n';
-        }
-    } 
